b582db83ac629eb1726be58e1eada9cd8e137e45
PankajAjmera1 <coderajmera@gmail.com>
2025-02-22 22:23:33 +0530

    Regex

---
+++ Regex/SpaceReplacer.java
@@ -0,0 +1,11 @@
+
+public class SpaceReplacer {
+    public String replaceSpaces(String input) {
+        return input.replaceAll("\\s+", " ");
+    }
+
+    public static void main(String[] args) {
+        SpaceReplacer replacer = new SpaceReplacer();
+        System.out.println(replacer.replaceSpaces("This is  an  example  with   multiple   spaces."));
+    }
+}
